0b5cb93b62908cd934c340618b0c8cf07f4cb18f
aime-byiringiro <aime.byiringiro@tcu.edu>
2024-05-03 15:35:48 -0500

    working on viewSection

--- src/components/Section/ViewSection.vue
+++ src/components/Section/ViewSection.vue
@@ -1,11 +1,52 @@
 <template>
 
+  <div>
+    <h1>{{section.sectionName}}</h1>
+    <h3>{{section.academicYear}}</h3>
+   <!-- <h3>{{details.firstDay}}</h3> -->
+  </div>
 </template>
+
+<script setup>
+</script>
 
 <script>
 
+import axios from "axios";
+
+export default {
+  name: "sectionDetails",
+  props: ['id'],
+  data() {
+    return {
+      section: {},
+    }
+    },
+  created() {
+    this.getDetails()
+  },
+
+  methods: {
+    getDetails () {
+      const URL = 'http://localhost:8080/section/' + this.id
+      axios.get(URL)
+          .then(response => {
+            this.section = response.data.data
+          })
+          .catch(error => {
+            console.error('There was an error!', error.response.data)
+          });
+    }
+  }
+
+}
+
 </script>
 
-<style>
+<style scoped>
+
+input{
+  margin: 10px;
+}
 
 </style>

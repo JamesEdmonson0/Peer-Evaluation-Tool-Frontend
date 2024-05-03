6418505f48e97d5d149887fda66c2f7fc1d10f2d
alsuri04 <aliya.suri@tcu.edu>
2024-05-03 11:48:12 -0500

    resolving conflicts

--- src/components/Team/TeamDetails.vue
+++ src/components/Team/TeamDetails.vue
@@ -1,3 +1,8 @@
+<script setup>
+import RemoveInstructorButton from './RemoveInstructorButton.vue';
+import InstructorSelector from './InstructorSelector.vue';
+</script>
+
 <template>
   <div>
     <h1>{{ team.teamName }}</h1>
@@ -8,7 +13,7 @@
     </div>
 
     <h3>Instructor</h3>
-    <div>{{ team.instructor.firstName }} {{ team.instructor.lastName }}</div>
+    <div><InstructorSelector :tid="id">{{ team.instructor.firstName }} {{ team.instructor.lastName }}</InstructorSelector><RemoveInstructorButton></RemoveInstructorButton></div>
 
   </div>
 </template>
@@ -35,6 +40,8 @@ export default {
       const URL = 'http://localhost:8080/teams/' + this.id
       axios.get(URL)
           .then(response => {
+            console.log(response.data.data)
+            this.details = response.data.data
             this.team = response.data.data
             console.log(this.team)
           })

5ee62d93f2674a6f100482749f3c7be88e1a0728
Sean Wymer <sean.wymer@tcu.edu>
2024-05-03 16:46:16 -0500

    added section filtering

---
+++ src/components/Utils/DisplaySection.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      Selected Section: <strong>{{ section }}</strong>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      section: String
+    }
+  };
+  </script>

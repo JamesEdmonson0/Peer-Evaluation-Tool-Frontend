1c095e2ead814980ed3c6923307614f4721e9a7b
Harrison Leath <hleath@me.com>
2024-05-04 14:00:25 -0500

    Added functionality to remove student from team

--- src/components/Team/TeamDetails.vue
+++ src/components/Team/TeamDetails.vue
@@ -3,12 +3,15 @@
     <h1>{{ team.teamName }}</h1>
 
     <h3>Students</h3>
-    <div v-for="(student, index) in students.get(team.id)" :key="index">
+    <button v-for="(student, index) in students.get(team.id)" :key="index" @click="removeStudent(student.id,
+                                          student.firstName, student.lastName)" style="margin-right: 10px">
       {{ student.firstName }} {{ student.lastName }}
-    </div>
+    </button>
 
     <h3>Instructor</h3>
     <div>{{ team.instructor.firstName }} {{ team.instructor.lastName }}</div>
+
+    <button class="btn-primary">Edit Team</button>
 
   </div>
 </template>
@@ -31,6 +34,28 @@ export default {
     this.getAllResults()
   },
   methods: {
+    async removeStudent(id, firstName, lastName) {
+      let result = confirm(`Are you sure you want to remove ${firstName} ${lastName} from team ${this.team.teamName}?`);
+      if (result === true) {
+        var modifiedStudents = JSON.parse(JSON.stringify(this.team.studentIds))
+        const idx = modifiedStudents.indexOf(id);
+        modifiedStudents.splice(idx, 1);
+        this.team.studentIds = modifiedStudents;
+
+        this.students = new Map();
+        await this.getAllResults();
+
+        const URL = "http://localhost:8080/teams/" + this.team.id;
+        axios.put(URL, this.team)
+            .then(response => {
+              console.log(response.data)
+              this.$emit('edited');
+            })
+            .catch(error => {
+              console.error('There was an error :(', error.response.data);
+            });
+      }
+    },
     async getDetails() {
       const URL = 'http://localhost:8080/teams/' + this.id
       axios.get(URL)
@@ -42,14 +67,11 @@ export default {
             console.error('There was an error.', error.response.data);
           });
     },
-
     async getAllResults() {
-      // await this.getDetails()
         for (const studentId of this.team.studentIds) {
           await this.getStudentById(studentId)
         }
     },
-
     async getStudentById(id) {
       const URL = `http://localhost:8080/students/${id}`;
       try {
@@ -88,9 +110,10 @@ input {
   margin: 10px;
 }
 
-button {
+.btn-primary {
   padding: 10px 20px;
-  margin-top: 20px;
+  margin-top: 15px;
+  margin-right: 10px;
   background-color: #4CAF50;
   color: white;
   border: none;
@@ -98,7 +121,7 @@ button {
   cursor: pointer;
 }
 
-button:hover {
+.btn-primary:hover {
   background-color: #45a049;
 }
 </style>
